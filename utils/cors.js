2c090c76ece8bed607326123c312d048fa920588
samspecial <psalmueloye@gmail.com>
2022-09-07 19:39:57 +0100

    email template modified

--- utils/cors.js
+++ utils/cors.js
@@ -1,5 +1,5 @@
 const cors = require("cors");
-const whitelist = ["http://localhost:1234", "http://localhost:4000"];
+const whitelist = ["http://localhost:1234", "http://localhost:3000"];
 
 const corsOptions = {
   optionsSuccessStatus: 200,
